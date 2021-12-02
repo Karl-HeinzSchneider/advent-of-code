4ef95d41a5e6be163b11ac262517b52efff196a6
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-02 04:06:06 +0100

    2020-02 Part1

--- years/2020/02/index.ts
+++ years/2020/02/index.ts
@@ -12,8 +12,66 @@ const DAY = 2;
 // data path    : C:\Users\Johannes\advent-of-code\years\2020\02\data.txt
 // problem url  : https://adventofcode.com/2020/day/2
 
+
+// helper
+interface pwType {
+	min: number,
+	max: number,
+	char: string,
+	pw: string
+}
+
+function convert(input: string): pwType[] {
+	const arr = input.split('\n');
+
+	return arr.map(entry => {
+		// ex: '5-6 s: zssmssbsms'
+		const arr = entry.split(' ');
+
+		// min-max  '5-6'
+		const arrMinMax = arr[0].split('-');
+		const min = arrMinMax[0];
+		const max = arrMinMax[1];
+
+		// char 's:'
+		const char = arr[1].slice(0, -1);
+
+		const pw = arr[2];
+
+		const typed: pwType = {
+			min: Number(min),
+			max: Number(max),
+			char: char,
+			pw: pw
+		}
+
+		return typed
+	});
+}
+
 async function p2020day2_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const arr = convert(input);
+
+	let validCount = 0;
+
+	arr.forEach(entry => {
+		const chars = entry.pw.split('');
+
+		let count = 0;
+
+		chars.forEach(char => {
+			if (char === entry.char) {
+				count = count + 1;
+			}
+		})
+
+		if (count >= entry.min && count <= entry.max) {
+			validCount = validCount + 1;
+		}
+
+	})
+
+	return validCount;
 }
 
 async function p2020day2_part2(input: string, ...params: any[]) {
@@ -21,7 +79,12 @@ async function p2020day2_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `1-3 a: abcde
+		1-3 b: cdefg
+		2-9 c: ccccccccc`,
+		expected: "2"
+	}];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
