e4cee966d8445ec0871e05f243caac97f97010fd
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-03 06:34:37 +0100

    2021-03 Part2

--- years/2021/03/index.ts
+++ years/2021/03/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { filter } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -46,23 +46,118 @@ async function p2021day3_part1(input: string, ...params: any[]) {
 			return '1';
 		}
 	})
-	console.log(gammaSplit);
-	console.log(epsilonSplit);
+	//console.log(gammaSplit);
+	//console.log(epsilonSplit);
 
 	const epsilonString = epsilonSplit.join('');
 	const gammaString = gammaSplit.join('');
 
-	console.log(epsilonString);
-	console.log(gammaString);
+	//console.log(epsilonString);
+	//console.log(gammaString);
 
-	const epsilonNr = parseInt(epsilonString,2);
+	const epsilonNr = parseInt(epsilonString, 2);
 	const gammaNr = parseInt(gammaString, 2);
 
-	return epsilonNr*gammaNr;
+	return epsilonNr * gammaNr;
+}
+
+function mostCommonBitAtIndex(input: string[], index: number): '0' | '1' {
+
+	let one = 0;
+	let zero = 0;
+
+	input.forEach(entry => {
+		if (entry.charAt(index) === '1') {
+			one = one + 1;
+		}
+		else {
+			zero = zero + 1;
+		}
+	})
+
+
+	if (one >= zero) {
+		return '1';
+	}
+	else {
+		return '0';
+	}
+}
+
+function leastCommonBitAtIndex(input: string[], index: number): '0' | '1' {
+
+	let one = 0;
+	let zero = 0;
+
+	input.forEach(entry => {
+		if (entry.charAt(index) === '1') {
+			one = one + 1;
+		}
+		else {
+			zero = zero + 1;
+		}
+	})
+
+
+	if (zero <= one) {
+		return '0';
+	}
+	else {
+		return '1';
+	}
 }
 
 async function p2021day3_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const arr = input.split('\n');
+
+	let oxygen = '';
+	let co2 = '';
+	// oxygen
+	let commonInit = mostCommonBitAtIndex(arr, 0);
+	let filtered = arr.filter(entry => entry.charAt(0) === commonInit);
+
+	//console.log('common: ' + commonInit + ' l: ' + filtered.length);
+
+	for (let i = 1; i < 12; i++) {
+
+		const common = mostCommonBitAtIndex(filtered, i);
+		filtered = filtered.filter(entry => entry.charAt(i) === common);
+
+		//console.log('common: ' + common + ' l: ' + filtered.length);
+
+
+		if (filtered.length === 1) {
+			console.log('finished Oxy: ' + filtered[0])
+			oxygen = filtered[0];
+			break;
+		}
+	}
+
+	// co2
+
+	commonInit = leastCommonBitAtIndex(arr, 0);
+	filtered = arr.filter(entry => entry.charAt(0) === commonInit);
+
+	for (let i = 1; i < 12; i++) {
+
+		const common = leastCommonBitAtIndex(filtered, i);
+		filtered = filtered.filter(entry => entry.charAt(i) === common);
+
+		//console.log('common: ' + common + ' l: ' + filtered.length);
+
+
+		if (filtered.length === 1) {
+			console.log('finished CO2: ' + filtered[0])
+			co2 = filtered[0];
+			break;
+		}
+	}
+
+
+	const oxygenNumber = parseInt(oxygen, 2);
+	const co2Number = parseInt(co2, 2);
+
+	return oxygenNumber * co2Number;
 }
 
 async function run() {
