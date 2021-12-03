4bf28d62aac7c1ca1a63d4694399a89863981381
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-03 04:32:27 +0100

    2020-06 Part1

--- years/2020/06/index.ts
+++ years/2020/06/index.ts
@@ -12,8 +12,50 @@ const DAY = 6;
 // data path    : C:\Users\Johannes\advent-of-code\years\2020\06\data.txt
 // problem url  : https://adventofcode.com/2020/day/6
 
+const testInput = `fbqjswm
+qmbjwfs
+fmsbjwq
+smjbqwf
+hwsqmbfj
+
+sxpoqbueg
+fbhxqzjrtdkgwaepos
+qbepgsxo
+pbxvosegq
+giqepcobsxl
+
+gd
+pfdy
+
+yijwkdzvoub
+cifujkyvbwz`
+
+function convert(input: string): string[][] {
+	const arr = input.split('\n\n').map(grp => grp.split('\n'));
+	return arr;
+}
+
 async function p2020day6_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const arr = convert(input);
+	//console.log(arr)
+
+	let count = 0;
+
+	arr.forEach(grp => {
+		let answers = new Set<string>();
+
+		grp.forEach(member => {			
+			for(let i = 0; i < member.length; i++){
+				answers.add(member.charAt(i));
+			}
+			
+		})
+
+		count = count + answers.size;
+		//console.log(answers);
+	})
+
+	return count;
 }
 
 async function p2020day6_part2(input: string, ...params: any[]) {
@@ -21,7 +63,13 @@ async function p2020day6_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+
+			input: testInput,
+			expected: "49"
+		}
+	];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
@@ -37,7 +85,7 @@ async function run() {
 		}
 	});
 	test.endTests();
-
+	//return;
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
 
