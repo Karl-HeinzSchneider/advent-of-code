c155dd1baae819e479f59d20319235449aa2f979
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-03 08:34:27 +0100

    2020-13 Part2

--- years/2020/13/index.ts
+++ years/2020/13/index.ts
@@ -35,21 +35,90 @@ async function p2020day13_part1(input: string, ...params: any[]) {
 		timestampLoop = timestampLoop + 1;
 	}
 
-	console.log(timestamp, busArray, maxValue);
+	//console.log(timestamp, busArray, maxValue);
 
-	console.log(timestampLoop - 1, busNr, wait);
+	//console.log(timestampLoop - 1, busNr, wait);
 
 
 	return wait * busNr;
 }
 
+const p2ex1 = `asfa
+67,7,59,61`;
+const p2ex2 = `asfa
+67,x,7,59,61`;
+const p2ex3 = `asfa
+67,7,x,59,61`;
+
 async function p2020day13_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const arr = input.split('\n');
+	const timestamp = Number(arr[0]);
+
+	const busArray = arr[1].split(',');
+
+	console.log(busArray);
+	let time = 0;
+
+	const maxTime = 100000000000000 * 100;
+	
+
+	// @TODO:
+	// Brutforce too slow -> Chinese Remainder Theorem, solution: 800177252346225
+	let magicNumber = 0;
+	while (magicNumber === 0) {
+
+		let guard = true;
+
+
+		for (let i = 0; i < busArray.length; i++) {
+			const item = busArray[i];
+			if (item === 'x') {
+
+			}
+			else {
+				const itemNumber = Number(item);
+
+				if ((time + i) % itemNumber === 0) {
+					// -> fine
+				}
+				else {
+					guard = false;
+				}
+			}
+		}
+
+
+		if (guard) {
+			console.log('FOUND')
+			magicNumber = time;
+		}
+
+		if (time > maxTime) {
+			magicNumber = 1;
+			console.log('FAILED')
+		}
+
+		time = time + 1;
+	}
+
+	console.log(magicNumber)
+	return magicNumber;
 }
 
 async function run() {
 	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part2tests: TestCase[] = [
+		{
+			input: p2ex1,
+			expected: "754018"
+		},
+		{
+			input:
+				`as
+13,14`,
+			expected: '13'
+		}
+	];
 
 	// Run tests
 	test.beginTests();
@@ -64,7 +133,7 @@ async function run() {
 		}
 	});
 	test.endTests();
-
+	//return;
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
 
