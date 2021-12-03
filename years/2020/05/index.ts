d3289c2d38ef3144ac5375c35d78786484c5b01f
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-03 02:11:33 +0100

    2020-05 Part1+2

--- years/2020/05/index.ts
+++ years/2020/05/index.ts
@@ -12,16 +12,109 @@ const DAY = 5;
 // data path    : C:\Users\Johannes\advent-of-code\years\2020\05\data.txt
 // problem url  : https://adventofcode.com/2020/day/5
 
+const testCodes = `BFFFBBFRRR
+FFFBBBFRRR
+BBFFBBFRLL`
+
+interface seat {
+	row: number,
+	column: number,
+	id: number
+}
+
+function decode(code: string): seat {
+
+	let charArray = code.split('');
+
+	let row = '';
+	for (let i = 0; i < 7; i++) {
+		const char = charArray[i];
+
+		if (char === 'F') {
+			row = row + '0';
+		}
+		else if (char === 'B') {
+			row = row + '1';
+		}
+	}
+
+	let column = '';
+	for (let i = 7; i < 11; i++) {
+		const char = charArray[i];
+
+		if (char === 'L') {
+			column = column + '0';
+		}
+		else if (char === 'R') {
+			column = column + '1';
+		}
+	}
+
+	const rowNumber = parseInt(row, 2);
+	const columnNumber = parseInt(column, 2);
+
+	const id = rowNumber * 8 + columnNumber;
+
+	//console.log('decode: ' + code);
+	//console.log('=  row: ' + row + ' column: ' + column);
+	//console.log('=  row: ' + rowNumber + ' column: ' + columnNumber);
+
+	return {
+		row: rowNumber,
+		column: columnNumber,
+		id: id
+	}
+}
+
+
+
 async function p2020day5_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const arr = input.split('\n');
+
+	let highest = 0;
+
+	arr.forEach(code => {
+		let tmp = decode(code);
+
+		if (tmp.id > highest) {
+			highest = tmp.id;
+		}
+		//console.log(tmp)
+	})
+
+	return highest;
 }
 
 async function p2020day5_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+
+	const arr = input.split('\n');
+
+	let seatSet = new Set<number>();
+
+	arr.forEach(code => {
+		let tmp = decode(code);
+		seatSet.add(tmp.id);
+		//console.log(tmp)
+	})
+
+	for (let i = 0; i < 848; i++) {
+		if (!seatSet.has(i)) {
+			if (seatSet.has(i - 1) && seatSet.has(i + 1)) {
+				return i;
+			}
+		}
+	}
+
+	return -1;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: testCodes,
+			expected: "820"
+		}
+	];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
@@ -37,7 +130,7 @@ async function run() {
 		}
 	});
 	test.endTests();
-
+	//return;
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
 
