eddb7ae95976c24ce3be76da716d3189e5f30b5d
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-07 02:49:03 +0100

    Update index.ts

--- years/2021/04/index.ts
+++ years/2021/04/index.ts
@@ -12,7 +12,77 @@ const DAY = 4;
 // data path    : C:\Users\Johannes\advent-of-code\years\2021\04\data.txt
 // problem url  : https://adventofcode.com/2021/day/4
 
+function convert(input: string) {
+	const arr = input.split('\n');
+
+	const bingoNumbers = arr[0].split(',').map(Number);
+
+	const newArr = arr.splice(1);
+	const length = newArr.length
+
+	let boards: number[][] = [];
+
+	for (let i = 0; i < length / 6; i++) {
+		const lineOne = newArr[i * 6 + 1].split(' ').map(Number);
+		const lineTwo = newArr[i * 6 + 2].split(' ').map(Number);
+		const lineThree = newArr[i * 6 + 3].split(' ').map(Number);
+		const lineFour = newArr[i * 6 + 4].split(' ').map(Number);
+		const lineFive = newArr[i * 6 + 5].split(' ').map(Number);
+
+		const board = [...lineOne, ...lineTwo, ...lineThree, ...lineFour, ...lineFive];
+		boards.push(board);
+	}
+
+	return { numbers: bingoNumbers, boards: boards };
+}
+
+function checkBoard(numbers: number[], board: number[]): boolean {
+
+	const sorted = numbers.sort((a, b) => a - b);
+
+
+	for (let i = 0; i < 5; i++) {
+		const lineSorted = board.slice(i, i + 5).sort((a, b) => a - b);
+		if (lineSorted === sorted) {
+			return true;
+		}
+
+		const columnSorted = [board[i], board[i + 5], board[i + 10], board[i + 15], board[i + 20]].sort((a, b) => a - b);
+
+		if (columnSorted === sorted) {
+			return true;
+		}
+		//console.log(sorted, lineSorted, columnSorted);
+
+	}
+
+	return false;
+}
+
+
 async function p2021day4_part1(input: string, ...params: any[]) {
+	const data = convert(input);
+
+	const bingoNumbers = data.numbers;
+	const boards = data.boards;
+
+	//console.log(bingoNumbers);
+	//console.log(boards[0]);
+
+	for (let i = 4; i < bingoNumbers.length; i++) {
+		const draw = bingoNumbers.slice(0, i + 1);
+
+		for (let j = 0; j < 1; j++) {
+			const board = boards[j];
+			const winning = checkBoard(draw, board);
+			if (winning) {
+				console.log(draw, j, board);
+			}
+		}
+
+
+	}
+
 	return "Not implemented";
 }
 
