e4a3a89c8b59226cfafcfdd1149b2ad890ead6c7
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-08 20:55:36 +0100

    2021-08 Part1
    383

--- years/2021/08/index.ts
+++ years/2021/08/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { uniq } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -12,8 +12,59 @@ const DAY = 8;
 // data path    : C:\Users\Johannes\advent-of-code\years\2021\08\data.txt
 // problem url  : https://adventofcode.com/2021/day/8
 
+function convert(input: string) {
+	const lines = input.split('\n');
+
+	let inputs: string[][] = [];
+	let outputs: string[][] = [];
+
+	lines.forEach(line => {
+		const split = line.split('|')
+		const input = split[0].split(' ').filter(line => line != '');
+		inputs.push(input);
+
+		const output = split[1].split(' ').filter(line => line != '');
+		outputs.push(output);
+	})
+
+	return [inputs, outputs];
+}
+
+// 0: abcefg  | 6
+// 1: cf	| 2
+// 2: acdeg | 5
+// 3: acdfg | 5
+// 4: bcdf	| 4
+// 5: abdfg	 | 5
+// 6: abdefg	| 6
+// 7: acf	| 3
+// 8: abcdefg | 7
+// 9: abcdfg	| 5
+
 async function p2021day8_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const arr = convert(input);
+	const inputs = arr[0];
+	const outputs = arr[1];
+
+	console.log(inputs[0])
+	console.log(outputs[0])
+	console.log(outputs[1])
+	console.log(outputs[2])
+
+
+	let sum = 0;
+	const unique = new Set([2, 3, 4, 7]);
+	outputs.forEach(put => {
+
+		put.forEach(segment => {
+			if (unique.has(segment.length)) {
+				sum = sum + 1;
+			}
+		})
+	})
+
+
+	return sum;
 }
 
 async function p2021day8_part2(input: string, ...params: any[]) {
