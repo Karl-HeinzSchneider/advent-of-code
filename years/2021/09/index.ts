59d3b008852c4a8826809c727af8c5ddcb39a0fb
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-13 06:03:15 +0100

    2021-09 Partt2
    1122700

--- years/2021/09/index.ts
+++ years/2021/09/index.ts
@@ -41,7 +41,6 @@ function getGridCoords(x: number, y: number, grid: number[]) {
 async function p2021day9_part1(input: string, ...params: any[]) {
 	const grid = convert(input);
 	const size = Math.sqrt(grid.length);
-	
 	let sum = 0;
 
 	for (let i = 0; i < size; i++) {
@@ -64,8 +63,76 @@ async function p2021day9_part1(input: string, ...params: any[]) {
 	return sum;
 }
 
+function getNeighbors(x: number, y: number, grid: number[]) {
+	let arr: { x: number, y: number, size: number }[] = [];
+
+	const up = getGridCoords(x, y + 1, grid);
+	arr.push({ x: x, y: y + 1, size: up });
+
+	const down = getGridCoords(x, y - 1, grid);
+	arr.push({ x: x, y: y - 1, size: down });
+
+	const left = getGridCoords(x - 1, y, grid);
+	arr.push({ x: x - 1, y: y, size: left });
+
+	const right = getGridCoords(x + 1, y, grid);
+	arr.push({ x: x + 1, y: y, size: right });
+
+	return arr;
+}
+
 async function p2021day9_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const grid = convert(input);
+	const size = Math.sqrt(grid.length);
+
+	let lowPoints: { x: number, y: number, size: number }[] = [];
+
+	for (let i = 0; i < size; i++) {
+		for (let j = 0; j < size; j++) {
+			const point = getGridCoords(i, j, grid);
+			//console.log(i,j,point)
+			const up = getGridCoords(i, j + 1, grid);
+			const down = getGridCoords(i, j - 1, grid);
+
+			const left = getGridCoords(i - 1, j, grid);
+			const right = getGridCoords(i + 1, j, grid);
+
+			if (point < up && point < down && point < left && point < right) {
+				lowPoints.push({ x: i, y: j, size: point });
+				//console.log('low:', i, j, point)
+			}
+		}
+	}
+
+	let basins: Set<string>[] = [];
+
+	lowPoints.forEach(low => {
+		let queue = [low];
+		let visited: Set<string> = new Set();
+
+		while (queue.length > 0) {
+			const point = queue.shift();
+			const neighbors = getNeighbors(point?.x!, point?.y!, grid);
+
+			neighbors.forEach(n => {
+				const id = `${n.x}|${n.y}`;
+				if (n.size < 9 && !visited.has(id)) {
+					queue.push(n);
+					visited.add(id);
+				}
+			})
+		}
+		basins.push(visited);
+	})
+
+	basins.sort((a, b) => b.size - a.size)
+	//console.log(basins);
+
+	const biggest = basins.slice(0, 3);
+	//console.log(biggest)
+
+	return biggest[0].size * biggest[1].size * biggest[2].size;
+
 }
 
 async function run() {
