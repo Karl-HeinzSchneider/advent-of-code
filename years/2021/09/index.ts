c0e9b18cbd770e9e6b4f09881c2c0c67263d03b5
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-13 05:19:01 +0100

    2021-09 Part1
    535

--- years/2021/09/index.ts
+++ years/2021/09/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { map } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -12,8 +12,56 @@ const DAY = 9;
 // data path    : C:\Users\Johannes\advent-of-code\years\2021\09\data.txt
 // problem url  : https://adventofcode.com/2021/day/9
 
+function convert(input: string): number[] {
+	let grid: number[] = [];
+	const arr = input.split('\n');
+	arr.forEach(line => {
+		const split = line.split('').map(Number);
+		split.forEach(nr => {
+			grid.push(nr);
+		})
+	})
+
+	return grid;
+}
+
+function getGridCoords(x: number, y: number, grid: number[]) {
+	// 100
+	const size = Math.sqrt(grid.length);
+
+	if (x < 0 || x > (size - 1) || y < 0 || y > (size - 1)) {
+		return 666;
+	}
+
+	const index = x + y * size;
+	//console.log(x, y, index)
+	return grid[index];
+}
+
 async function p2021day9_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const grid = convert(input);
+	const size = Math.sqrt(grid.length);
+	
+	let sum = 0;
+
+	for (let i = 0; i < size; i++) {
+		for (let j = 0; j < size; j++) {
+			const point = getGridCoords(i, j, grid);
+			//console.log(i,j,point)
+			const up = getGridCoords(i, j + 1, grid);
+			const down = getGridCoords(i, j - 1, grid);
+
+			const left = getGridCoords(i - 1, j, grid);
+			const right = getGridCoords(i + 1, j, grid);
+
+			if (point < up && point < down && point < left && point < right) {
+				sum = sum + 1 + point;
+				//console.log('low:', i, j, point)
+			}
+		}
+	}
+
+	return sum;
 }
 
 async function p2021day9_part2(input: string, ...params: any[]) {
