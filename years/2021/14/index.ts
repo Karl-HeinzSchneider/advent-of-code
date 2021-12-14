29cf9b8a6f577e24a01d2b3ca4be5c533598bf3d
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-14 07:49:38 +0100

    2021-14 Part1
    2712

--- years/2021/14/index.ts
+++ years/2021/14/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { replace } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -11,9 +11,95 @@ const DAY = 14;
 // solution path: C:\Users\Johannes\advent-of-code\years\2021\14\index.ts
 // data path    : C:\Users\Johannes\advent-of-code\years\2021\14\data.txt
 // problem url  : https://adventofcode.com/2021/day/14
+const example = `NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`;
+
+function convert(input: string) {
+	const arr = input.split('\n');
+
+	const template = arr[0];
+
+	const replaceMap: Map<string, string> = new Map();
+
+	arr.slice(2).forEach(line => {
+		const split = line.split(' -> ');
+		const first = split[0];
+		const second = split[1];
+
+		replaceMap.set(first, second);
+	})
+
+	return { template: template, map: replaceMap };
+}
+
+function poly(input: string, mapper: Map<string, string>): string {
+
+	let substrArr: string[] = [];
+
+	for (let i = 0; i < input.length - 1; i++) {
+		substrArr.push(input[i] + input[i + 1]);
+	}
+	const newPolyString = substrArr.map(str => {
+		const newElem = mapper.get(str)!;
+		return str[0] + newElem;
+	}).join('') + input[input.length - 1];
+
+	return newPolyString;
+}
 
 async function p2021day14_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const conv = convert(input);
+
+	const template = conv.template;
+	const replaceMap = conv.map;
+
+	console.log(replaceMap)
+
+	const steps = 10;
+	let polyString = template;
+	console.log(0, polyString.length, polyString);
+
+	for (let i = 1; i <= steps; i++) {
+		polyString = poly(polyString, replaceMap);
+		//console.log(i, polyString.length, polyString);
+	}
+
+	let charMap: Map<string, number> = new Map();
+	polyString.split('').forEach(char => {
+		if (!charMap.has(char)) {
+			charMap.set(char, 1);
+		}
+		else {
+			const old = charMap.get(char)!;
+			charMap.set(char, old + 1);
+		}
+	})
+	console.log(charMap)
+
+	const max = [...charMap.entries()].reduce((a, e) => e[1] > a[1] ? e : a);
+
+	const min = [...charMap.entries()].reduce((a, e) => e[1] < a[1] ? e : a);
+
+	const diff = max[1] - min[1];
+	console.log(min, max, diff);
+
+	return diff;
 }
 
 async function p2021day14_part2(input: string, ...params: any[]) {
@@ -21,7 +107,7 @@ async function p2021day14_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{ input: example, expected: '1588' }];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
@@ -37,7 +123,7 @@ async function run() {
 		}
 	});
 	test.endTests();
-
+	//return;
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
 
