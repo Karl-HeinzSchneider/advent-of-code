a4309498f6a70d198feee8ec950c50fc92e03255
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-14 06:35:32 +0100

    2021-12 Part 1
    4104

--- years/2021/12/index.ts
+++ years/2021/12/index.ts
@@ -12,8 +12,89 @@ const DAY = 12;
 // data path    : C:\Users\Johannes\advent-of-code\years\2021\12\data.txt
 // problem url  : https://adventofcode.com/2021/day/12
 
+const example = `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`;
+
+function convert(input: string) {
+	const arr = input.split('\n');
+
+	const adjMap: Map<string, string[]> = new Map();
+
+	arr.forEach(line => {
+		const split = line.split('-');
+		const first = split[0]!;
+		const second = split[1]!;
+
+		if (!adjMap.has(first)) {
+			adjMap.set(first, [second]);
+		}
+		else {
+			const firstArr = adjMap.get(first)!;
+			adjMap.set(first, [...firstArr, second]);
+		}
+
+		if (!adjMap.has(second)) {
+			adjMap.set(second, [first]);
+		}
+		else {
+			const secondArr = adjMap.get(second)!;
+			adjMap.set(second, [...secondArr, first]);
+		}
+	})
+
+	return adjMap;
+}
+
+function isSmall(input: string) {
+	return input.toLowerCase() === input;
+}
+
+function findAllPaths(start: string, end: string, adjMap: Map<string, string[]>) {
+
+	const BFS = function (current: string, target: string, visited: string[]) {
+		const newVisited = [...visited, current];
+
+		if (current === target) {
+			paths.push(newVisited);
+			//console.log(newVisited);
+			return;
+		}
+
+		const adjArr = adjMap.get(current)!;
+
+		adjArr.forEach(adj => {
+			if (isSmall(adj)) {
+				if (!newVisited.includes(adj)) {
+					BFS(adj, target, newVisited);
+				}
+			}
+			else {
+				BFS(adj, target, newVisited);
+			}
+		})
+	}
+	let paths: string[][] = [];
+
+	BFS(start,end,[]);
+
+	return paths;
+}
+
 async function p2021day12_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const adjMap = convert(input);
+	//console.log(adjMap);
+
+	const start = 'start';
+	const end = 'end';
+
+	const paths = findAllPaths(start, end, adjMap);
+
+	return paths.length;
 }
 
 async function p2021day12_part2(input: string, ...params: any[]) {
@@ -21,7 +102,7 @@ async function p2021day12_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{ input: example, expected: '10' }];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
@@ -37,7 +118,7 @@ async function run() {
 		}
 	});
 	test.endTests();
-
+	//return;
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
 
