7fbe290c789b8bd52c982c5c94d9056d264fa090
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2021-12-14 05:06:22 +0100

    2021-10 Part1
    469755

--- years/2021/10/index.ts
+++ years/2021/10/index.ts
@@ -12,8 +12,93 @@ const DAY = 10;
 // data path    : C:\Users\Johannes\advent-of-code\years\2021\10\data.txt
 // problem url  : https://adventofcode.com/2021/day/10
 
+const example =
+	`[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]`;
+
+function isLineCorrupt(input: string) {
+	const openSet = new Set(['(', '[', '{', '<']);
+	const closeSet = new Set([')', ']', '}', '>']);
+
+	const mapper: Map<string, string> = new Map();
+
+	for (let i = 0; i < openSet.size; i++) {
+		const open = [...openSet];
+		const close = [...closeSet];
+		mapper.set(close[i], open[i]);
+	}
+
+	const split = input.split('');
+
+	let charsToParse: string[] = split;
+	let stack: string[] = [];
+
+	while (charsToParse.length > 0) {
+		//console.log(stack.join(''), ' --- ', charsToParse.join(''));
+
+		const currentChar = charsToParse.shift()!;
+
+		// open Char => new Chunk
+		if (openSet.has(currentChar)) {
+			stack.push(currentChar);
+		}
+		// close Char
+		else if (closeSet.has(currentChar)) {
+
+			const lastStack = stack.pop();
+
+			// stack empty and close Char => invalid
+			if (!lastStack) {
+				return { corrupt: true, firstIncorrect: currentChar };
+			}
+
+			const matching = mapper.get(currentChar)!;
+			if (lastStack === matching) {
+
+			}
+			// ex: ( + ]
+			else {
+				return { corrupt: true, firstIncorrect: currentChar };
+			}
+
+		}
+	}
+
+	return { corrupt: false, firstIncorrect: '' };
+}
+
 async function p2021day10_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const arr = input.split('\n');
+
+	const scoreMap: Map<string, number> = new Map();
+	scoreMap.set(')', 3);
+	scoreMap.set(']', 57);
+	scoreMap.set('}', 1197);
+	scoreMap.set('>', 25137);
+
+
+	let score = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		const line = arr[i];
+		const isCorrupt = isLineCorrupt(line);
+
+		const mapped = scoreMap.get(isCorrupt.firstIncorrect);
+		const lineScore = mapped ? mapped : 0;
+		console.log(isCorrupt, lineScore)
+
+		score = score + lineScore;
+	}
+
+	return score;
 }
 
 async function p2021day10_part2(input: string, ...params: any[]) {
@@ -21,7 +106,7 @@ async function p2021day10_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{ input: example, expected: '26397' }];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
@@ -37,7 +122,7 @@ async function run() {
 		}
 	});
 	test.endTests();
-
+	//return;
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
 
