f2ff4734ccc1d9335c6f845209ccefae3510df6c
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-12-09 14:38:29 +0100

    2022-05 Part1
    FCVRLMVQP

--- years/2022/05/index.ts
+++ years/2022/05/index.ts
@@ -12,8 +12,98 @@ const DAY = 5;
 // data path    : C:\Users\Johannes\advent-of-code\years\2022\05\data.txt
 // problem url  : https://adventofcode.com/2022/day/5
 
+interface crateStack {
+	number: number,
+	crates: string[]
+}
+
+interface instruction {
+	amount: number,
+	from: number,
+	to: number
+}
+
+function readCratesObj(input: string): { stack: crateStack[], emptyIndex: number, instructions: instruction[] } {
+	const arr = input.split('\n')
+	const emptyIndex = arr.findIndex(x => x == '')
+
+	const numberLine = arr[emptyIndex - 1]
+	console.log(emptyIndex, numberLine)
+
+	let crates: crateStack[] = []
+	crates.push({ number: 0, crates: [] })
+	for (let i = 0; i < numberLine.length; i++) {
+		const str = numberLine[i]
+		if (str != ' ') {
+			let crate: crateStack = {
+				number: Number(str),
+				crates: []
+			}
+
+			for (let j = emptyIndex - 2; j >= 0; j--) {
+				const cr: string = arr[j][i]
+				if (cr != ' ') {
+					crate.crates.push(cr)
+				}
+			}
+			crates.push(crate)
+		}
+	}
+
+	return { stack: crates, emptyIndex: emptyIndex, instructions: readInstructions(arr.slice(emptyIndex + 1)) }
+}
+
+function readInstructions(input: string[]): instruction[] {
+	let arr: instruction[] = []
+
+	input.forEach(str => {
+		const split = str.replace('move', '').replace('from', '').replace('to', '').split(' ').filter(x => x != '').map(x => Number(x))
+		arr.push({
+			amount: split[0],
+			from: split[1],
+			to: split[2]
+		})
+	})
+
+	return arr;
+}
+
+function getCratesOntop(state: crateStack[]): string {
+	let str: string = ''
+
+	state.forEach(s => {
+		str = str + s.crates[s.crates.length - 1]
+	})
+
+	return str;
+}
+
 async function p2022day5_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const crateObj = readCratesObj(input)
+	//console.log(crateObj.emptyIndex)
+	//console.log(crateObj.stack)
+	console.log(crateObj.instructions)
+
+
+	let state = crateObj.stack
+	let intructions = crateObj.instructions
+
+	intructions.forEach(instr => {
+		for (let i = 1; i <= instr.amount; i++) {
+			const crate = state[instr.from].crates.pop()
+			if (crate) {
+				state[instr.to].crates.push(crate!)
+			}
+		}
+	})
+
+	//console.log(state)
+
+	const crateString = getCratesOntop(state.slice(1))
+	//console.log(crateString)
+
+
+	return crateString;
 }
 
 async function p2022day5_part2(input: string, ...params: any[]) {
