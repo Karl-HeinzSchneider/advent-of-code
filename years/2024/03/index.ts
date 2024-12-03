20b4510f2d7e9b0838168dd67a04f596ca25b6dd
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-12-03 22:55:45 +0100

    2024-03 Part2

--- years/2024/03/index.ts
+++ years/2024/03/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { split } from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
@@ -13,7 +13,6 @@ const DAY = 3;
 // problem url  : https://adventofcode.com/2024/day/3
 
 function multiplyMatch(input: string): number {
-
 	const split = input.split(',');
 
 	const firstNumberStr = split[0].replace('mul(', '');
@@ -37,13 +36,40 @@ async function p2024day3_part1(input: string, ...params: any[]) {
 	return score;
 }
 
+function multiplyMatchesInString(input: string): number {
+	const mulRegex = /mul\(\d*,\d*\)/g
+	const matches = [...input.matchAll(mulRegex)].map(m => m[0]);
+
+	let score = 0;
+	matches.forEach(m => {
+		score = score + multiplyMatch(m)
+	})
+
+	return score
+}
+
 async function p2024day3_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	let score = 0;
+	const splitDont = input.split(`don't()`);
+
+	// start is always enabled
+	score = score + multiplyMatchesInString(splitDont[0]);
+
+	// check each section after a `don't()`
+	for (let i = 1; i < splitDont.length; i++) {
+		const splits = splitDont[i].split(`do()`)
+		// discard the first one, as it's after a `don't()` and before a `do()`
+		for (let j = 1; j < splits.length; j++) {
+			score = score + multiplyMatchesInString(splits[j])
+		}
+	}
+
+	return score;
 }
 
 async function run() {
 	const part1tests: TestCase[] = [{ input: 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))', expected: "161" }];
-	const part2tests: TestCase[] = [];
+	const part2tests: TestCase[] = [{ input: `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`, expected: "48" }];
 
 	// Run tests
 	test.beginTests();
