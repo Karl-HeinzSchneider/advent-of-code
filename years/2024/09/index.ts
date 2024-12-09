042e2c19ae26315a5a9e0ce80df392f75909b565
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-12-09 22:09:55 +0100

    2024-09 Part1

--- years/2024/09/index.ts
+++ years/2024/09/index.ts
@@ -12,8 +12,84 @@ const DAY = 9;
 // data path    : E:\Projects\advent-of-code\years\2024\09\data.txt
 // problem url  : https://adventofcode.com/2024/day/9
 
+// first try, returns checksum
+function defrag(input: string): number {
+	let score = 0;
+
+	const inputLength = input.length
+
+	let blockArr: number[] = []
+	let visibleString = ''
+
+	let id = 0;
+	for (let i = 0; i < inputLength; i++) {
+		const currentBlock = Number(input.charAt(i));
+		// log(currentChar) 
+		if (i % 2 == 0) {
+			// file
+			for (let k = 0; k < currentBlock; k++) {
+				blockArr.push(id);
+				visibleString = visibleString + id
+			}
+			id = id + 1;
+		}
+		else {
+			// space
+			for (let k = 0; k < currentBlock; k++) {
+				blockArr.push(-1);
+				visibleString = visibleString + '.'
+			}
+		}
+	}
+
+	// log(blockArr)
+	// log(visibleString)
+
+	// let firstPointer = 0;
+	// let secondPointer = blockArr.length - 1;
+
+	// while (true) {
+	// 	while (blockArr[firstPointer] != -1) {
+	// 		firstPointer = firstPointer + 1;
+	// 	}
+	// }
+
+	function printBlockArrAsString(arr: number[]) {
+		log(arr.join('').replaceAll('-1', '.'));
+	}
+
+	const blockArrLen = blockArr.length
+	// printBlockArrAsString(blockArr)
+	for (let i = blockArrLen - 1; i > 0; i--) {
+		const currentBlock = blockArr[i];
+		const firstIndex = blockArr.indexOf(-1);
+		if (firstIndex < i) {
+			blockArr[firstIndex] = currentBlock;
+			blockArr[i] = -1
+			// printBlockArrAsString(blockArr)
+		}
+		else {
+			break;
+		}
+	}
+
+	for (let i = 0; i < blockArrLen; i++) {
+		const currentBlock = blockArr[i];
+		if (currentBlock < 0) {
+			return score;
+		}
+		else {
+			score = score + i * currentBlock
+		}
+	}
+
+	return score;
+}
+
 async function p2024day9_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const score = defrag(input);
+
+	return score;
 }
 
 async function p2024day9_part2(input: string, ...params: any[]) {
@@ -21,7 +97,7 @@ async function p2024day9_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{ input: `2333133121414131402`, expected: "1928" }];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
