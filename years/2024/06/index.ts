c43011e1bedf659d4c10b66101854c2d5c1956f7
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-12-09 06:09:09 +0100

    2024-06 Part1

--- years/2024/06/index.ts
+++ years/2024/06/index.ts
@@ -12,8 +12,105 @@ const DAY = 6;
 // data path    : E:\Projects\advent-of-code\years\2024\06\data.txt
 // problem url  : https://adventofcode.com/2024/day/6
 
+function createGrid(input: string): { grid: string[][], start: { x: number, y: number } } {
+	let grid: string[][] = [];
+	const rows = input.split('\n');
+	let start: { x: number, y: number } = { x: -1, y: -1 };
+
+	for (let i = 0; i < rows.length; i++) {
+		const row = rows[i];
+
+		const indexOfStart = row.indexOf('^')
+		if (indexOfStart > 0) {
+			// log('index:', i, indexOfStart)
+			start = { x: i, y: indexOfStart }
+		}
+
+		const split = row.split('')
+		grid.push(split)
+	}
+
+	return { grid: grid, start: start };
+}
+
+function isInsideGrid(pos: { x: number, y: number }, width: number, height: number): boolean {
+	if (pos.x < 0 || pos.x >= width) {
+		return false;
+	}
+	if (pos.y < 0 || pos.y >= height) {
+		return false;
+	}
+
+	return true;
+}
+
 async function p2024day6_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const { grid, start } = createGrid(input);
+	log('Found start at: ', start)
+
+	const width = grid[0].length;
+	const height = grid.length;
+	log('w:', width, ', h:', height)
+
+	type GuardDirections = '^' | '>' | 'v' | '<'
+	const guardDict = {
+		'^': { x: -1, y: 0 },
+		'>': { x: 0, y: 1 },
+		'v': { x: 1, y: 0 },
+		'<': { x: 0, y: -1 },
+	}
+	const guardDictNextMove = {
+		'^': '>',
+		'>': 'v',
+		'v': '<',
+		'<': '^'
+	}
+
+	let guardDir: GuardDirections = '^'
+	let guard = start;
+	let score = 1;
+
+	grid[guard.x][guard.y] = 'X'
+
+	if (!isInsideGrid(guard, width, height)) {
+		return 0;
+	}
+	// if (true) return 0;
+
+	while (true) {
+		const guardDeltaPos = guardDict[guardDir]
+		const nextPosition = { x: guard.x + guardDeltaPos.x, y: guard.y + guardDeltaPos.y }
+		// log('current:', guard, 'next:', nextPosition)
+
+		if (!isInsideGrid(nextPosition, width, height)) {
+			return score;
+		}
+
+		const nextStr = grid[nextPosition.x][nextPosition.y];
+
+		if (nextStr == '.') {
+			// normal field
+			// log('-> normal field')
+			guard = nextPosition;
+			grid[guard.x][guard.y] = 'X'
+			score = score + 1;
+		}
+		else if (nextStr == 'X') {
+			// normal field but already visited
+			// log('-> visited field')
+			guard = nextPosition;
+		}
+		else if (nextStr == '#') {
+			// obstacle
+			// log('-> BLOCKED')
+			guardDir = guardDictNextMove[guardDir] as GuardDirections;
+		}
+		else {
+			// log('fail??');
+			return score;
+		}
+		// log('~~~~~~~~~~~~~')
+	}
 }
 
 async function p2024day6_part2(input: string, ...params: any[]) {
@@ -21,7 +118,21 @@ async function p2024day6_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input:
+				`....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`, expected: "41"
+		}
+	];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
@@ -37,6 +148,7 @@ async function run() {
 		}
 	});
 	test.endTests();
+	// if (true) return;
 
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
