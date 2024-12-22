eecb83de832c519483e88a86f7add7583ef4715f
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-12-22 20:03:49 +0100

    2024-15 Part2

--- years/2024/15/index.ts
+++ years/2024/15/index.ts
@@ -127,8 +127,8 @@ async function p2024day15_part1(input: string, ...params: any[]) {
 		// log('After:', robot)
 	}
 
-	log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-	log('Robot after:', robot)
+	// log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	// log('Robot after:', robot)
 
 	let score = 0;
 
@@ -147,7 +147,396 @@ async function p2024day15_part1(input: string, ...params: any[]) {
 }
 
 async function p2024day15_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const gridMap = new Map<string, string>();
+	const split = input.split('\n\n')
+	const rows = split[0].split('\n');
+
+	let robot = [-1, -1]
+
+	for (let i = 0; i < rows.length; i++) {
+		const row = rows[i];
+
+		for (let j = 0; j < row.length; j++) {
+			const current = row[j];
+
+			if (current === '@') {
+				robot = [i, 2 * j]
+				gridMap.set(coordString(i, 2 * j), '@')
+				gridMap.set(coordString(i, 2 * j + 1), '.')
+			}
+			else if (current === 'O') {
+				gridMap.set(coordString(i, 2 * j), '[')
+				gridMap.set(coordString(i, 2 * j + 1), ']')
+			}
+			else if (current === '.') {
+				gridMap.set(coordString(i, 2 * j), '.')
+				gridMap.set(coordString(i, 2 * j + 1), '.')
+			}
+			else if (current === '#') {
+				gridMap.set(coordString(i, 2 * j), '#')
+				gridMap.set(coordString(i, 2 * j + 1), '#')
+			}
+		}
+	}
+
+	const movementArr: string[] = [];
+
+	split[1].split('\n').forEach(l => {
+		l.split('').forEach(c => {
+			movementArr.push(c);
+		})
+	})
+
+	function printGridMap() {
+		for (let i = 0; i < rows.length; i++) {
+			const row = rows[i];
+
+			let rowStr = '';
+
+			for (let j = 0; j < 2 * row.length; j++) {
+				rowStr = rowStr + getCoord(i, j);
+			}
+			log(rowStr)
+		}
+	}
+
+	// log(gridMap)
+	printGridMap()
+	// log(movementArr);
+	log(robot)
+
+	// log('----------------------------------')
+
+	// 
+	function areCoordsValid(x: number, y: number) {
+		return gridMap.has(coordString(x, y,))
+	}
+
+	function getCoord(x: number, y: number) {
+		return gridMap.get(coordString(x, y))
+	}
+
+	function setCoord(x: number, y: number, value: string) {
+		return gridMap.set(coordString(x, y), value)
+	}
+
+	type GuardDirections = '^' | '>' | 'v' | '<'
+	const guardDict = {
+		'^': { x: -1, y: 0 },
+		'>': { x: 0, y: 1 },
+		'v': { x: 1, y: 0 },
+		'<': { x: 0, y: -1 },
+	}
+
+	for (let i = 0; i < movementArr.length; i++) {
+		const move = movementArr[i] as GuardDirections;
+		const deltaVector = guardDict[move];
+		const oldRobot = robot;
+		// log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+		// log('Move:', oldRobot, move, deltaVector)
+
+		let newX = oldRobot[0] + deltaVector.x
+		let newY = oldRobot[1] + deltaVector.y
+
+		// must exist because robot is inside at the start and wont move out of the room
+		const newPos = getCoord(newX, newY)!
+
+		if (newPos === '#') {
+			// wall -> do nothing
+		}
+		else if (newPos === '.') {
+			// empty spot -> move robot
+			setCoord(newX, newY, '@');
+			setCoord(oldRobot[0], oldRobot[1], '.');
+			robot = [newX, newY]
+		}
+		else {
+			// O -> try to move
+
+			// <<<<  ,  >>>>  ~> more or less same as Part1
+			if (move === '<' || move === '>') {
+				let moveX = newX + deltaVector.x
+				let moveY = newY + deltaVector.y
+				let counter = 0;
+
+				while (areCoordsValid(moveX, moveY)) {
+					const movePos = getCoord(moveX, moveY)!; // must exist because valid
+					counter = counter + 1;
+
+					if (movePos === '#') {
+						// wall -> cant move
+						break;
+					}
+					else if (movePos === '.') {
+						// empty spot found -> move
+
+						// move chests to empty spot
+						// setCoord(moveX, moveY, 'O')
+
+						for (let i = 0; i < counter; i++) {
+							const char = getCoord(moveX - deltaVector.x * (i + 1), moveY - deltaVector.y * (i + 1))!
+							setCoord(moveX - deltaVector.x * i, moveY - deltaVector.y * i, char)
+						}
+
+						// move robot
+						setCoord(newX, newY, '@');
+						setCoord(oldRobot[0], oldRobot[1], '.');
+						robot = [newX, newY]
+						// log(gridMap)
+						// log(robot)
+						// return;
+						break;
+					}
+					else {
+						// other O -> try more positions
+					}
+					moveX = moveX + deltaVector.x
+					moveY = moveY + deltaVector.y
+				}
+
+			}
+			// ^^^^^  
+			else if (move === '^') {
+				// deltaVector { x: -1, y: 0 }				
+				let changesArr: [x: number, y: number, str: string][] = []
+				let moveValid = true;
+
+				function moveUp(x: number, y: number) {
+					// log('moveUp', x, y)
+					if (!moveValid) {
+						// log('!moveValid')
+						return
+					}
+
+					const current = getCoord(x, y)!
+					// log('current:', current, ' @', x, y)
+					// if (current == '.') {
+					// 	return;
+					// }
+
+					let left: number[] = []
+					let right: number[] = []
+
+					if (current == '[') {
+						left = [x, y]
+						right = [x, y + 1]
+					}
+					else if (current == ']') {
+						left = [x, y - 1]
+						right = [x, y]
+					}
+
+					const topLeft = getCoord(left[0] - 1, left[1])!;
+					const topRight = getCoord(right[0] - 1, right[1])!;
+
+					if (topLeft == '#' || topRight == '#') {
+						// ~> cant move
+						moveValid = false;
+						return;
+					}
+
+					// if (x == newX && y == newY) {
+					// 	// first box ~> make space
+					// 	// changesArr.push([left[0], left[1], '.']);
+					// 	// changesArr.push([right[0], right[1], '.']);
+					// }
+
+					changesArr.push([left[0], left[1], '.']);
+					changesArr.push([right[0], right[1], '.']);
+
+					changesArr.push([left[0] - 1, left[1], '[']);
+					changesArr.push([right[0] - 1, right[1], ']']);
+
+					if (topLeft == '[' && topRight == ']') {
+						// box ontop ~> move
+						moveUp(left[0] - 1, left[1])
+					}
+
+					if (topLeft == '.') {
+						// topLeft empty ~> do nothing
+					}
+					else if (topLeft == ']') {
+						// topLeft box right side ~> move
+						moveUp(left[0] - 1, left[1])
+					}
+
+					if (topRight == '.') {
+						// topRight empty ~> do nothing
+					}
+					else if (topRight == '[') {
+						// topRight box right side ~> move
+						moveUp(right[0] - 1, right[1])
+					}
+				}
+
+				moveUp(newX, newY);
+
+				// log(changesArr)
+				// printGridMap()
+				// return;
+
+				if (moveValid) {
+					// update changes
+					changesArr.reverse().forEach(c => {
+						if (c[2] != '.') {
+							setCoord(c[0], c[1], c[2]);
+						}
+						else {
+							const sameCoords = changesArr.filter(x => x[0] === c[0] && x[1] === c[1])
+							const notEmpty = sameCoords.filter(x => x[2] === '[' || x[2] === ']')
+							if (notEmpty.length > 0) {
+								// skip
+							}
+							else {
+								setCoord(c[0], c[1], c[2]);
+							}
+						}
+					})
+
+					// move robot
+					setCoord(newX, newY, '@');
+					setCoord(oldRobot[0], oldRobot[1], '.');
+					robot = [newX, newY]
+
+					// printGridMap()
+					// return;
+				}
+			}
+			// vvvvv 
+			else if (move === 'v') {
+				// deltaVector { x: 1, y: 0 }				
+				let changesArr: [x: number, y: number, str: string][] = []
+				let moveValid = true;
+
+				function moveDown(x: number, y: number) {
+					// log('moveDown', x, y)
+					if (!moveValid) {
+						// log('!moveValid')
+						return
+					}
+
+					const current = getCoord(x, y)!
+					// log('current:', current, ' @', x, y)
+					// if (current == '.') {
+					// 	return;
+					// }
+
+					let left: number[] = []
+					let right: number[] = []
+
+					if (current == '[') {
+						left = [x, y]
+						right = [x, y + 1]
+					}
+					else if (current == ']') {
+						left = [x, y - 1]
+						right = [x, y]
+					}
+
+					const bottomLeft = getCoord(left[0] + 1, left[1])!;
+					const bottomRight = getCoord(right[0] + 1, right[1])!;
+
+					if (bottomLeft == '#' || bottomRight == '#') {
+						// ~> cant move
+						moveValid = false;
+						return;
+					}
+
+					// if (x == newX && y == newY) {
+					// 	// first box ~> make space
+					// 	// changesArr.push([left[0], left[1], '.']);
+					// 	// changesArr.push([right[0], right[1], '.']);
+					// }
+
+					changesArr.push([left[0], left[1], '.']);
+					changesArr.push([right[0], right[1], '.']);
+
+					changesArr.push([left[0] + 1, left[1], '[']);
+					changesArr.push([right[0] + 1, right[1], ']']);
+
+					if (bottomLeft == '[' && bottomRight == ']') {
+						// box below ~> move
+						moveDown(left[0] + 1, left[1])
+					}
+
+					if (bottomLeft == '.') {
+						// bottomLeft empty ~> do nothing
+					}
+					else if (bottomLeft == ']') {
+						// bottomLeft box right side ~> move
+						moveDown(left[0] + 1, left[1])
+					}
+
+					if (bottomRight == '.') {
+						// bottomRight empty ~> do nothing
+					}
+					else if (bottomRight == '[') {
+						// bottomRight box right side ~> move
+						moveDown(right[0] + 1, right[1])
+					}
+				}
+
+				moveDown(newX, newY);
+
+				// log(changesArr)
+				// printGridMap()
+				// return;
+
+				if (moveValid) {
+					// update changes				
+					changesArr.reverse().forEach(c => {
+						if (c[2] != '.') {
+							setCoord(c[0], c[1], c[2]);
+						}
+						else {
+							const sameCoords = changesArr.filter(x => x[0] === c[0] && x[1] === c[1])
+							const notEmpty = sameCoords.filter(x => x[2] === '[' || x[2] === ']')
+							if (notEmpty.length > 0) {
+								// skip
+							}
+							else {
+								setCoord(c[0], c[1], c[2]);
+							}
+						}
+					})
+
+					// move robot
+					setCoord(newX, newY, '@');
+					setCoord(oldRobot[0], oldRobot[1], '.');
+					robot = [newX, newY]
+
+					// printGridMap()
+					// return;
+				}
+			}
+		}
+
+
+		// log(gridMap)
+		// printGridMap()
+		// log('After:', robot)
+	}
+
+	log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	log('Robot after:', robot)
+	printGridMap()
+
+	let score = 0;
+
+	for (let i = 0; i < rows.length; i++) {
+		const row = rows[i];
+
+		for (let j = 0; j < 2 * row.length; j++) {
+			const current = getCoord(i, j)!
+			if (current === '[') {
+				score = score + 100 * i + j
+			}
+		}
+	}
+
+
+	log('~~>> ', score, ' <<~~')
+	return score;
 }
 
 async function run() {
@@ -163,7 +552,94 @@ async function run() {
 
 <^^>>>vv<v>>v<<`, expected: "2028"
 	}];
-	const part2tests: TestCase[] = [];
+	const part2tests: TestCase[] = [
+		{
+			input: `#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^`, expected: "618"
+		},
+		{
+			input: `##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`, expected: "9021"
+		},
+		{
+			input: `#######
+#.....#
+#.....#
+#.@O..#
+#..#O.#
+#...O.#
+#..O..#
+#.....#
+#######
+
+>><vvv>v>^^^`, expected: "1430"
+		},
+		{
+			input: `#######
+#.....#
+#.OO@.#
+#.....#
+#######
+
+<<`, expected: "406"
+		},
+		{
+			input: `#######
+#.....#
+#.O#..#
+#..O@.#
+#.....#
+#######
+
+<v<<^`, expected: "509"
+		},
+		{
+			input: `#######
+#.....#
+#.#O..#
+#..O@.#
+#.....#
+#######
+
+<v<^`, expected: "511"
+		},
+		{
+			input: `######
+#....#
+#.O..#
+#.OO@#
+#.O..#
+#....#
+######
+
+<vv<<^`, expected: "816"
+		}];
 
 	// Run tests
 	test.beginTests();
