51d04cece664fc44c71090f9f976d1c6f2a86c83
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-12-22 10:27:15 +0100

    2024-12 Part2

--- years/2024/12/index.ts
+++ years/2024/12/index.ts
@@ -145,7 +145,221 @@ async function p2024day12_part1(input: string, ...params: any[]) {
 }
 
 async function p2024day12_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	// square
+	const n = input.indexOf('\n')
+	// log('garden plot size:', n, 'x', n)
+
+	const gridMap = new Map<string, string>();
+	const rows = input.split('\n');
+
+	for (let i = 0; i < rows.length; i++) {
+		const row = rows[i];
+
+		for (let j = 0; j < row.length; j++) {
+			gridMap.set(coordString(i, j), row[j])
+		}
+	}
+	// log(gridMap)
+
+	function areCoordsValid(x: number, y: number) {
+		return gridMap.has(coordString(x, y,))
+	}
+
+	function getCoord(x: number, y: number) {
+		return gridMap.get(coordString(x, y))
+	}
+
+	function setCoord(x: number, y: number, value: string) {
+		return gridMap.set(coordString(x, y), value)
+	}
+
+	const dxdy: number[][] = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+	function floodFill(i: number, j: number) {
+		if (!areCoordsValid(i, j)) {
+			log('Floodfill', '(', i, ',', j, ')', 'type:', 'INVALID COORDS')
+		}
+		const plantType = getCoord(i, j)!; // must exist because valid coords
+		// log('Floodfill', '(', i, ',', j, ')', 'type:', plantType)
+
+		let area = 0;
+		let borders = 0;
+
+		let coords: [x: number, y: number, d1: boolean, d2: boolean, d3: boolean, d4: boolean][] = [];
+
+		const Q: [x: number, y: number][] = [];
+		Q.push([i, j])
+
+		while (Q.length > 0) {
+			const [x, y] = Q.pop()!; // must exist because Q.length > 0
+			// log('~~~~~~~~~~~~~~~~~~~~~')
+			// log('Queue:', x, y)
+
+			if (!areCoordsValid(x, y) || getCoord(x, y) != plantType) {
+				continue;
+			}
+
+			area = area + 1;
+			setCoord(x, y, plantType.toLowerCase())
+
+			let currentBorders = 0;
+
+			let borderDs: boolean[] = [];
+
+			dxdy.forEach(([dx, dy]) => {
+				// log('**search dir', dx, dy, '**')
+				let newX = x + dx;
+				let newY = y + dy;
+				// log('dxdy', newX, newY)
+
+				if (!areCoordsValid(newX, newY)) {
+					// log('->', 'INVALID COORDS')
+					currentBorders = currentBorders + 1;
+					borderDs.push(true);
+				}
+				else {
+					const otherPlantType = getCoord(newX, newY);
+
+					if (otherPlantType == plantType) {
+						// log('->', 'SAME PLANT = PUSH')
+						Q.push([newX, newY])
+						borderDs.push(false);
+					}
+					else if (otherPlantType == plantType.toLowerCase()) {
+						// log('->', 'SAME PLANT, BUT ALREADY VISITED')
+						borderDs.push(false);
+					}
+					else {
+						// log('->', 'OTHER PLANT')
+						currentBorders = currentBorders + 1;
+						borderDs.push(true);
+					}
+				}
+			})
+			// log('-->> Borders:', currentBorders)
+			borders = borders + currentBorders;
+			coords.push([x, y, borderDs[0], borderDs[1], borderDs[2], borderDs[3]])
+		}
+
+		// log(area, borders, area * borders)
+
+		// log(area, borders)
+		// log(coords)
+
+		function combineBorders(arr: [x: number, y: number, d1: boolean, d2: boolean, d3: boolean, d4: boolean][]): [x: number, y: number, d1: boolean, d2: boolean, d3: boolean, d4: boolean][] {
+			// log('combineBorders', arr)
+
+			const coordMap = new Map<string, [d1: boolean, d2: boolean, d3: boolean, d4: boolean]>();
+			for (let i = 0; i < arr.length; i++) {
+				const [x, y, d1, d2, d3, d4] = arr[i];
+				coordMap.set(coordString(x, y), [d1, d2, d3, d4]);
+			}
+
+			// log(coordMap)
+
+			// const dxdy: number[][] = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+			for (let i = 0; i < arr.length; i++) {
+				const [x, y] = arr[i];
+
+				const coordStr = coordString(x, y);
+				const coordObj = coordMap.get(coordStr)!;
+				const [d1, d2, d3, d4] = coordObj;
+
+				// log('~~~~~~~')
+				// log('coord:', x, y)
+
+				// d1 = top border, d2 = bottom border
+				// d3 = left border, d4 = right border
+				const searchDirsArr = [
+					[[0, -1], [0, 1]], // <-- , -->
+					[[0, -1], [0, 1]], // <-- , -->
+					[[-1, 0], [1, 0]], // ^ , v
+					[[-1, 0], [1, 0]]  // ^ , v
+				]
+				const logStr = ['top', 'bottom', 'left', 'right']
+
+				for (let d = 0; d < 4; d++) {
+					if (coordObj[d]) {
+						// log('-----border:', logStr[d])
+						const dIndex = d;
+						const searchDirs = searchDirsArr[dIndex] // <-- , -->
+
+						searchDirs.forEach(dir => {
+							// log('-> dir', dir)
+							let newX = x + dir[0];
+							let newY = y + dir[1];
+
+							let newCoordStr = coordString(newX, newY);
+
+							while (coordMap.has(newCoordStr)) {
+								let newCoordObj = coordMap.get(newCoordStr)!;
+								// log('-->> check', newX, newY)
+								if (newCoordObj[dIndex]) {
+									newCoordObj[dIndex] = false;
+									coordMap.set(newCoordStr, newCoordObj);
+
+									newX = newX + dir[0]
+									newY = newY + dir[1]
+									newCoordStr = coordString(newX, newY);
+								}
+								else {
+									// exit loop
+									newCoordStr = coordString(-1, -1);
+								}
+							}
+						})
+					}
+					else {
+						// log('-----no border:', logStr[d])
+					}
+				}
+			}
+
+			let newArr: [x: number, y: number, d1: boolean, d2: boolean, d3: boolean, d4: boolean][] = []
+			for (let i = 0; i < arr.length; i++) {
+				const [x, y, d1, d2, d3, d4] = arr[i];
+
+				let newCoordObj = coordMap.get(coordString(x, y))!;
+
+				// log('old:', arr[i])
+				// log('new:', newCoordObj)
+
+				newArr.push([x, y, ...newCoordObj])
+			}
+
+			return newArr;
+		}
+
+		const combinedBorders = combineBorders(coords);
+		// log('combineBorders', combinedBorders)
+
+		borders = 0;
+		combinedBorders.forEach(b => {
+			borders = borders + b.filter(x => x === true).length;
+		})
+
+		// log(area, borders, area * borders)
+
+		return area * borders;
+	}
+
+	// floodFill(0, 0);
+
+	let score = 0;
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < n; j++) {
+			const plantType = getCoord(i, j)!;
+			if (plantType == '*' || plantType == plantType.toLowerCase()) {
+				// already visited
+				continue;
+			}
+			const f = floodFill(i, j)
+			score = score + f;
+		}
+	}
+
+	return score;
 }
 
 async function run() {
@@ -166,7 +380,28 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE`, expected: "1930"
 	}];
-	const part2tests: TestCase[] = [];
+	const part2tests: TestCase[] = [
+		// {
+		// 		input: `AAAA
+		// BBCD
+		// BBCC
+		// EEEC`, expected: "80"
+		// 	},
+		// {
+		// 	input: `OOOOO
+		// OXOXO
+		// OOOOO
+		// OXOXO
+		// OOOOO`, expected: "436"
+		// },
+		{
+			input: `EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE`, expected: "236"
+		}
+	];
 
 	// Run tests
 	test.beginTests();
