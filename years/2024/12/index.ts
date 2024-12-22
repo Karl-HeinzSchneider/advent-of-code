f091676639e6c7f7a6882956a25b6bdf717b2f64
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-12-22 07:19:48 +0100

    2024-12 Part1

--- years/2024/12/index.ts
+++ years/2024/12/index.ts
@@ -14,16 +14,29 @@ const DAY = 12;
 
 interface Region { id: number, coords: string[] }
 
+function coordString(x: number, y: number) {
+	return `${x}-${y}`
+}
+
 async function p2024day12_part1(input: string, ...params: any[]) {
 	// square
 	const n = input.indexOf('\n')
 	log('garden plot size:', n, 'x', n)
 
-	const gridString = input.replaceAll('\n', '');
+	// const gridString = input.replaceAll('\n', '');
 	// log(gridString.length)
 
-	const regionMap = new Map<number, Region>();
-	const gridMap = new Map<string, number>();
+	const gridMap = new Map<string, string>();
+	const rows = input.split('\n');
+
+	for (let i = 0; i < rows.length; i++) {
+		const row = rows[i];
+
+		for (let j = 0; j < row.length; j++) {
+			gridMap.set(coordString(i, j), row[j])
+		}
+	}
+	// log(gridMap)
 
 	let regionCount = 0;
 	function getNewRegion(): Region {
@@ -32,32 +45,103 @@ async function p2024day12_part1(input: string, ...params: any[]) {
 		return { id: regionCount, coords: [] }
 	}
 
-	function floodFill(i: number, j: number) {
-		log('Floodfill', i, j)
+	function areCoordsValid(x: number, y: number) {
+		// if (x < 0 || x >= n || y < 0 || y >= n) {
+		// 	return false;
+		// }
+		// else {
+		// 	return true;
+		// }
+		return gridMap.has(coordString(x, y,))
+	}
 
-		const plantType = gridString.charAt(i * n + j);
-		log(plantType)
+	function getCoord(x: number, y: number) {
+		return gridMap.get(coordString(x, y))
+	}
+
+	function setCoord(x: number, y: number, value: string) {
+		return gridMap.set(coordString(x, y), value)
+	}
+
+	const dxdy: number[][] = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+
+	function floodFill(i: number, j: number) {
+		if (!areCoordsValid(i, j)) {
+			// log('Floodfill', '(', i, ',', j, ')', 'type:', 'INVALID COORDS')
+		}
+		const plantType = getCoord(i, j)!; // must exist because valid coords
+		// log('Floodfill', '(', i, ',', j, ')', 'type:', plantType)
+
+		// log(plantType)
+
+		let area = 0;
+		let borders = 0;
 
 		const Q: [x: number, y: number][] = [];
 		Q.push([i, j])
 
 		while (Q.length > 0) {
-			const entry = Q.pop()!;
-			const x = entry[0];
-			const y = entry[1];
+			const [x, y] = Q.pop()!; // must exist because Q.length > 0
+			// log('~~~~~~~~~~~~~~~~~~~~~')
+			// log('Queue:', x, y)
 
-			if (x < 0 || x >= n || y < 0 || y >= n) {
+			if (!areCoordsValid(x, y) || getCoord(x, y) != plantType) {
 				continue;
 			}
 
+			area = area + 1;
+			setCoord(x, y, plantType.toLowerCase())
+
+			let currentBorders = 0;
+
+			dxdy.forEach(([dx, dy]) => {
+				const newX = x + dx;
+				const newY = y + dy;
+				// log('dxdy', newX, newY)
+
+				if (!areCoordsValid(newX, newY)) {
+					// log('->', 'INVALID COORDS')
+					currentBorders = currentBorders + 1;
+				}
+				else {
+					const otherPlantType = getCoord(newX, newY);
+
+					if (otherPlantType == plantType) {
+						// log('->', 'SAME PLANT = PUSH')
+						Q.push([newX, newY])
+					}
+					else if (otherPlantType == plantType.toLowerCase()) {
+						// log('->', 'SAME PLANT, BUT ALREADY VISITED')
+					}
+					else {
+						// log('->', 'OTHER PLANT')
+						currentBorders = currentBorders + 1;
+					}
+				}
+			})
+			// log('-->> Borders:', currentBorders)
+			borders = borders + currentBorders;
+		}
+
+		// log(area, borders, area * borders)
+		return area * borders;
+	}
+
+	// floodFill(0, 0);
+
+	let score = 0;
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < n; j++) {
+			const plantType = getCoord(i, j)!;
+			if (plantType == '*' || plantType == plantType.toLowerCase()) {
+				// already visited
+				continue;
+			}
+			score = score + floodFill(i, j);
 		}
 	}
 
-	floodFill(0, 0);
-
-
-
-	return "Not implemented";
+	return score;
 }
 
 async function p2024day12_part2(input: string, ...params: any[]) {
@@ -69,7 +153,18 @@ async function run() {
 		input: `AAAA
 BBCD
 BBCC
-EEEC`, expected: "772"
+EEEC`, expected: "140"
+	}, {
+		input: `RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE`, expected: "1930"
 	}];
 	const part2tests: TestCase[] = [];
 
@@ -91,14 +186,14 @@ EEEC`, expected: "772"
 	const input = await util.getInput(DAY, YEAR);
 
 	const part1Before = performance.now();
-	// const part1Solution = String(await p2024day12_part1(input));
+	const part1Solution = String(await p2024day12_part1(input));
 	const part1After = performance.now();
 
 	const part2Before = performance.now()
 	const part2Solution = String(await p2024day12_part2(input));
 	const part2After = performance.now();
 
-	// logSolution(12, 2024, part1Solution, part2Solution);
+	logSolution(12, 2024, part1Solution, part2Solution);
 
 	log(chalk.gray("--- Performance ---"));
 	log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
