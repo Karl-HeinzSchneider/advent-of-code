529eb7641f2108b2c5d0c1c239a7d7f407327d1e
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-12-29 22:01:50 +0100

    2024-23 Part1

--- years/2024/23/index.ts
+++ years/2024/23/index.ts
@@ -12,8 +12,64 @@ const DAY = 23;
 // data path    : E:\Projects\advent-of-code\years\2024\23\data.txt
 // problem url  : https://adventofcode.com/2024/day/23
 
+interface AdjacencyGraph {
+	[keyof: string]: Set<string>;
+}
+
 async function p2024day23_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const split = input.split('\n')
+
+	let graph: AdjacencyGraph = {};
+
+	split.forEach(s => {
+		const [pc1, pc2] = s.split('-');
+
+		if (graph[pc1]) {
+			graph[pc1].add(pc2);
+		}
+		else {
+			graph[pc1] = new Set([pc2]);
+		}
+
+		if (graph[pc2]) {
+			graph[pc2].add(pc1);
+		}
+		else {
+			graph[pc2] = new Set([pc1]);
+		}
+	})
+
+	// log(graph)
+	// log(Object.keys(graph))
+
+	const computerList = Object.keys(graph)
+	log('Computers: ', computerList.length);
+
+	let foundTriples = new Set<string>();
+
+	computerList.forEach(c => {
+		const neighbors = graph[c];
+
+		neighbors.forEach(n => {
+			const otherNeighbors = graph[n];
+
+			otherNeighbors.forEach(o => {
+				if (neighbors.has(o) && graph[o].has(c)) {
+					// log('found:', c, n, o)
+					const arr = [c, n, o].sort()
+					const tripleStr = arr.join(',')
+					// log('found:', foundStr)
+					if (c.startsWith('t') || n.startsWith('t') || o.startsWith('t')) {
+						foundTriples.add(tripleStr)
+					}
+				}
+			})
+		})
+	})
+
+	// log([...foundTriples].sort())
+
+	return foundTriples.size;
 }
 
 async function p2024day23_part2(input: string, ...params: any[]) {
@@ -21,7 +77,40 @@ async function p2024day23_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn`, expected: "7"
+	}];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
