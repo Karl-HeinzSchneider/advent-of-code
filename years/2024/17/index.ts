f47f16f29a31cf57e98414dfa226198f55a30cce
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-01-03 18:01:26 +0100

    2024-17 Part1

--- years/2024/17/index.ts
+++ years/2024/17/index.ts
@@ -13,7 +13,187 @@ const DAY = 17;
 // problem url  : https://adventofcode.com/2024/day/17
 
 async function p2024day17_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const split = input.split('\n');
+
+	let A = Number(split[0].split(':')[1]);
+	let B = Number(split[1].split(':')[1]);
+	let C = Number(split[2].split(':')[1]);
+
+	const program = split[4].split(': ')[1].split(',').map(x => Number(x));
+	const programLen = program.length;
+
+	log('Register A:', A);
+	log('Register B:', B);
+	log('Register C:', C);
+	log('Program:', program);
+
+	let output: number[] = [];
+
+	let instructionPointer = 0;
+	let literalOperant = 0;
+	let comboOperant = 0;
+
+	// 0 - a division
+	function adv(combo: number) {
+		// log('..adv', combo)
+		let tmp = (A / Math.pow(2, combo));
+		tmp = Math.trunc(tmp)
+		A = tmp;
+		log('..adv', combo, 'tmp:', tmp)
+	}
+
+	// 1 - bitwise XOR
+	function bxl(literal: number) {
+		// log('..bxl', literal)
+
+		let tmp = B ^ literal;
+		B = tmp;
+		log('..bxl', literal, 'tmp:', tmp)
+	}
+
+	// 2 - mod 8
+	function bst(combo: number) {
+		// log('..bst', combo)
+		let tmp = combo % 8;
+		B = tmp;
+		log('..bst', combo, 'tmp:', tmp)
+	}
+
+	// 3 - jump
+	function jnz(literal: number) {
+		log('..jnz', literal, 'A:', A)
+
+		if (A === 0) {
+			return false;
+		}
+		else {
+			instructionPointer = literal;
+			return true;
+		}
+	}
+
+	// 4 -
+	function bxc(operand: number) {
+		// log('..bxc', operand)
+		let tmp = B ^ C;
+		B = tmp;
+		log('..bxc', '(', operand, ')', 'tmp:', tmp)
+		// instructionPointer = instructionPointer 
+	}
+
+	// 5 - output
+	function out(combo: number) {
+		log('..out', combo)
+		let tmp = combo % 8;
+		output.push(tmp);
+		log('~~>> OUT:', tmp)
+	}
+
+	// 6 - b division
+	function bdv(combo: number) {
+		// log('..bdv', combo)
+		let tmp = (A / Math.pow(2, combo));
+		tmp = Math.trunc(tmp)
+		B = tmp;
+		log('..bdv', combo, 'tmp:', tmp)
+	}
+
+	// 7 - c division
+	function cdv(combo: number) {
+		// log('..cdv', combo)
+		let tmp = (A / Math.pow(2, combo));
+		tmp = Math.trunc(tmp)
+		C = tmp;
+		log('..cdv', combo, 'tmp:', tmp)
+	}
+
+	while (instructionPointer < programLen && instructionPointer + 1 < programLen) {
+		log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+		const opcode = program[instructionPointer];
+		const operand = program[instructionPointer + 1];
+
+		literalOperant = operand;
+
+		let isJumping = false;
+
+		if (operand <= 3) {
+			comboOperant = operand;
+		}
+		else if (operand == 4) {
+			comboOperant = A
+		}
+		else if (operand == 5) {
+			comboOperant = B
+		}
+		else if (operand == 6) {
+			comboOperant = C
+		}
+		else if (operand == 7) {
+			// log('Register A:', A);
+			// log('Register B:', B);
+			// log('Register C:', C);
+			// log('opcode', opcode, 'operand', operand);
+			// log('literal', literalOperant, 'combo', comboOperant)
+			log('~~~~~>>>> SHOULD NOT HAPPEN <<<<~~~~ 7')
+
+			// return;
+		}
+		else {
+			// log('Register A:', A);
+			// log('Register B:', B);
+			// log('Register C:', C);
+			// log('opcode', opcode, 'operand', operand);
+			// log('literal', literalOperant, 'combo', comboOperant)
+			log('~~~~~>>>> SHOULD NOT HAPPEN <<<<~~~~ else')
+			// return;
+		}
+
+		log('Register A:', A);
+		log('Register B:', B);
+		log('Register C:', C);
+		log('opcode', opcode, 'operand', operand);
+		log('literal', literalOperant, 'combo', comboOperant)
+
+		switch (opcode) {
+			case 0:
+				adv(comboOperant);
+				break;
+			case 1:
+				bxl(literalOperant);
+				break;
+			case 2:
+				bst(comboOperant);
+				break;
+			case 3:
+				isJumping = jnz(literalOperant);
+				break;
+			case 4:
+				bxc(-1);
+				break;
+			case 5:
+				out(comboOperant);
+				break;
+			case 6:
+				bdv(comboOperant);
+				break;
+			case 7:
+				cdv(comboOperant);
+				break;
+		}
+
+
+		if (isJumping) {
+			// set inside jnz
+		}
+		else {
+			instructionPointer = instructionPointer + 2
+		}
+	}
+
+	const outputStr = output.join(',')
+	log('====', outputStr)
+
+	return outputStr;
 }
 
 async function p2024day17_part2(input: string, ...params: any[]) {
@@ -21,7 +201,46 @@ async function p2024day17_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0`,
+		expected: "4,6,3,5,6,3,5,2,1,0"
+	},
+	{
+		input: `Register A: 0
+Register B: 0
+Register C: 9
+
+Program: 2,6`,
+		expected: ''
+	},
+	{
+		input: `Register A: 10
+Register B: 0
+Register C: 0
+
+Program: 5,0,5,1,5,4`,
+		expected: '0,1,2'
+	},
+	{
+		input: `Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0`,
+		expected: '4,2,5,6,7,7,7,7,3,1,0'
+	},
+	{
+		input: `Register A: 0
+Register B: 29
+Register C: 0
+
+Program: 1,7`,
+		expected: ''
+	}];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
